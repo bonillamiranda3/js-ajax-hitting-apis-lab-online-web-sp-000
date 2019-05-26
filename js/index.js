0cb4e57bb6b6c3dc5f8b131e44a7562c41a00c4f
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-05-26 05:40:00 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -7,7 +7,30 @@ function getRepositories() {
   req.send();
 }
 
-function displayRepositories() {\
+  function displayRepositories() {
   const repos = JSON.parse(this.responseText);
   const repoList = `<ul>${repos.map(r => '<li>' + `<a href= ${r.html_url}>${r.name}</a>` + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoList;
 }
+
+  function getCommits(el) {
+    const repo = el.dataset.repository;
+    req.addEventListener('load', displayCommits)
+    req.open('GET', 'https://api.github.com/repos/' + el.dataset.username + '/' + repo + '/commits');
+    req.send();
+  }
+
+  function displayCommits() {
+    const commits = JSON.parse(this.responseText);
+    const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.commit.author.name + '</strong> - ' + commit.committer.login  + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`;
+    document.getElementById('details').innerHTML = commitsList;  
+    }
+
+  function getBranches(el) {
+    const repo = el.dataset.repository;
+    const username = document.getElementById('username').value
+    const req = new XMLHttpRequest();
+    req.addEventListener('load', displayBranches)
+    req.open('GET', 'https://api.github.com/repos/' + el.dataset.username + '/' + repo + '/commits');
+    req.send();
+  }
