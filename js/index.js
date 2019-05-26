3df33820fec31365a5d08a6b138b24c23a109886
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-05-26 05:56:03 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -15,6 +15,8 @@ function displayRepositories() {
 
   function getCommits(el) {
     const repo = el.dataset.repository;
+    const username = document.getElementById('username').value
+    const req = new XMLHttpRequest();
     req.addEventListener('load', displayCommits)
     req.open('GET', 'https://api.github.com/repos/' + el.dataset.username + '/' + repo + '/commits');
     req.send();
