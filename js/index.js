604a1c43f3962bd3b32b4e983688422a186457df
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-05-26 03:46:58 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // your code here
+function getRepositories() {
+  const username = document.getElementById('username').value
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayRepositories);
+  req.open('GET', `https://api.github.com/users/${username}/repos`);
+  req.send();
+}
+
+function displayRepositories() {\
+  const repos = JSON.parse(this.responseText);
+  const repoList = `<ul>${repos.map(r => '<li>' + `<a href= ${r.html_url}>${r.name}</a>` + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`;
+}
